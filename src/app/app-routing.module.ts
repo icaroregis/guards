b832aa59e6caab9d9d6a18ad6f8ca43fc832a545
Ícaro Almeida <62527468+icaroregis@users.noreply.github.com>
2022-09-19 15:25:28 -0300

    montando modulos, componentes e rotas do projeto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {
+  //   path: '',
+  //   component: HomeComponent,
+  // },
+  // {
+  //   path: 'account',
+  //   component: AccountComponent,
+  //   canActivate: [CanActiveGuard],
+  // },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
